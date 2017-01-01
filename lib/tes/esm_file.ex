40e848cb02a14019af266e2ac5ce86bb0069e32c
Rebecca Skinner <traybaby@gmail.com>
2017-01-01 16:01:02 +0800

    Add some smoke tests around the data being read, using a sample ESM file
    Also clean up warnings and minor tidy-ups

--- lib/tes/esm_file.ex
+++ lib/tes/esm_file.ex
@@ -13,27 +13,24 @@ defmodule Tes.EsmFile do
     )
   end
 
-  @doc """
-  The ESM/ESP/ESS files are composed entirely of records with the following format:
-
-  Record
-	4 bytes: char Name[4]
-		4-byte record name string (not null-terminated)
-	4 bytes: long Size
-		Size of the record not including the 16 bytes of header data.
-	4 bytes: long Header1
-		Unknown value, usually 0 (deleted/ignored flag?).
-	4 bytes: long Flags
-		Record flags.
-			 0x00002000 = Blocked
-			 0x00000400 = Persistant
-	? bytes: SubRecords[]
-		All records are composed of a variable number of sub-records. There
-		is no sub-record count, just use the record Size value to determine
-		when to stop reading a record.
-
-  File format taken from http://www.uesp.net/morrow/tech/mw_esm.txt
-  """
+  # The ESM/ESP/ESS files are composed entirely of records with the following format:
+  #
+  # 4 bytes: char Name[4]
+  #    4-byte record name string (not null-terminated)
+  # 4 bytes: long Size
+  #   Size of the record not including the 16 bytes of header data.
+  # 4 bytes: long Header1
+  #   Unknown value, usually 0 (deleted/ignored flag?).
+  # 4 bytes: long Flags
+  #   Record flags.
+  #      0x00002000 = Blocked
+  #      0x00000400 = Persistant
+  # ? bytes: SubRecords[]
+  #   All records are composed of a variable number of sub-records. There
+  #   is no sub-record count, just use the record Size value to determine
+  #   when to stop reading a record.
+  #
+  # File format taken from http://www.uesp.net/morrow/tech/mw_esm.txt
   defp next_record(file) do
     case IO.binread(file, 16) do
       :eof -> {:halt, file}
@@ -48,20 +45,17 @@ defmodule Tes.EsmFile do
   defp build_record("FACT", subrecords), do: Tes.EsmFormatter.faction(subrecords)
   defp build_record(type, subrecords), do: %{type: type, subrecords: subrecords}
 
-  @doc """
-  All records, as shown above, are again composed entirely of a variable number of
-  sub-records with a similar format, as given below:
-
-  Sub-Record
-	4 bytes: char Name[4]
-		4-byte sub-record name string (not null-terminated)
-	4 bytes: long Size
-		Size of the sub-record not including the 8 bytes of header data.
-	? bytes: Sub-Record data.
-		Format depends on the sub-record type (see below).
-
-  File format taken from http://www.uesp.net/morrow/tech/mw_esm.txt
-  """
+  # All records, as shown above, are again composed entirely of a variable number of
+  # sub-records with a similar format, as given below:
+  #
+  # 4 bytes: char Name[4]
+  #   4-byte sub-record name string (not null-terminated)
+  # 4 bytes: long Size
+  #   Size of the sub-record not including the 8 bytes of header data.
+  # ? bytes: Sub-Record data.
+  #   Format depends on the sub-record type (see below).
+  #
+  # File format taken from http://www.uesp.net/morrow/tech/mw_esm.txt
   defp parse_sub_records("", _type, list), do: list
   defp parse_sub_records(<<name::binary-size(4), size::little-integer-size(32), rest::binary>>, type, list) do
     # Split the "rest" out into the content for this sub-record, and the rest
@@ -72,9 +66,6 @@ defmodule Tes.EsmFile do
     parse_sub_records(rest, type, new_list)
   end
 
-  @doc """
-  Later down the track a function head can be added for lists of properties, eg. NPC-held items or dialogue conditions
-  """
   defp record_value(list, "FACT", "RNAM", value), do: Map.update(list, "RNAM", [value], &(&1 ++ [value]))
 
   # ANAM and INTV subrecords come in pairs and should be stored together
@@ -88,32 +79,29 @@ defmodule Tes.EsmFile do
 
   defp record_value(list, _type, name, value), do: Map.put_new(list, name, value)
 
-  @doc """
-  For type-specific formatting.
-  eg. some fields are null-terminated strings, some are bitmasks, some are little-endian integers
-  """
-  defp format_value("FACT", name, value) when name in ["FNAM", "NAME", "RNAM"], do: strip_null(value)
+  # For type-specific formatting.
+  # eg. some fields are null-terminated strings, some are bitmasks, some are little-endian integers
+  defp format_value("FACT", name, value) when name in ["FNAM", "NAME", "RNAM", "ANAM"], do: strip_null(value)
   defp format_value("FACT", "INTV", <<value::signed-little-integer-size(32)>>), do: value
   # Rankings and skills are long and repeated - split them out into their own functions
   defp format_value("FACT", "FADT", <<
     attribute_1::little-integer-size(32), attribute_2::little-integer-size(32),
     rankings::binary-size(200), skills::binary-size(24),
-    unknown::little-integer-size(32),
-    flags::signed-little-integer-size(32)>>) do
-      %{attribute_ids: [attribute_1, attribute_2], rankings: format_faction_rankings(rankings),
-        skill_ids: format_faction_skills(skills), unknown: unknown, flags: flags}
+    unknown::little-integer-size(32), flags::signed-little-integer-size(32)>>) do
+      %{attribute_ids: [attribute_1, attribute_2], rankings: faction_rankings(rankings),
+        skill_ids: faction_skills(skills), unknown: unknown, flags: flags}
   end
 
   defp format_value("BOOK", name, value) when name in ["NAME", "MODL", "FNAM", "ITEX", "SCRI", "ENAM"], do: strip_null(value)
   defp format_value("BOOK", "BKDT", <<weight::little-float-size(32),
-    value::little-integer-size(32), scroll::little-integer-size(32), skill_id::signed-little-integer-size(32),
-    enchantment::little-integer-size(32)>>) do
+    value::little-integer-size(32), scroll::little-integer-size(32),
+    skill_id::signed-little-integer-size(32), enchantment::little-integer-size(32)>>) do
       skill_id = if skill_id > 0, do: skill_id, else: nil
       %{weight: weight, value: value, scroll: scroll == 1, skill_id: skill_id, enchantment: enchantment}
   end
   defp format_value("BOOK", "TEXT", value) do
     # 147 and 148 are Windows-specific smart quotes - replace with Unicode quotes
-    value |> String.replace(<<147>>, "“") |> String.replace(<<148>>, "”")
+    value |> strip_null |> String.replace(<<147>>, "“") |> String.replace(<<148>>, "”")
   end
 
   defp format_value("SKIL", "INDX", <<size::little-integer-size(32)>>), do: size
@@ -126,21 +114,19 @@ defmodule Tes.EsmFile do
 
   defp strip_null(name), do: String.split(name, <<0>>, parts: 2) |> List.first
 
-  defp format_faction_skills(skills), do: format_faction_skills(skills, [])
-  defp format_faction_skills("", list), do: Enum.reverse(list)
-  defp format_faction_skills(<<skill::signed-little-integer-size(32), rest::binary>>, list) do
-    case skill do
-      -1 -> list
-      skill -> format_faction_skills(rest, [skill | list])
-    end
+  defp faction_skills(skills), do: faction_skills(skills, [])
+  defp faction_skills("", list), do: Enum.reverse(list)
+  defp faction_skills(<<-1::signed-little-integer-size(32), _rest::binary>>, list), do: list
+  defp faction_skills(<<skill::signed-little-integer-size(32), rest::binary>>, list) do
+    faction_skills(rest, [skill | list])
   end
 
-  defp format_faction_rankings(rankings), do: format_faction_rankings(rankings, [])
-  defp format_faction_rankings("", list), do: Enum.reverse(list)
-  defp format_faction_rankings(<<attribute_1::little-integer-size(32), attribute_2::little-integer-size(32),
+  defp faction_rankings(rankings), do: faction_rankings(rankings, [])
+  defp faction_rankings("", list), do: Enum.reverse(list)
+  defp faction_rankings(<<attribute_1::little-integer-size(32), attribute_2::little-integer-size(32),
     skill_1::little-integer-size(32), skill_2::little-integer-size(32), faction::little-integer-size(32),
     rest::binary>>, list) do
-      format_faction_rankings(rest, [%{attribute_1: attribute_1, attribute_2: attribute_2, skill_1: skill_1,
+      faction_rankings(rest, [%{attribute_1: attribute_1, attribute_2: attribute_2, skill_1: skill_1,
         skill_2: skill_2, faction: faction} | list])
   end
 end
--- test/tes_test.exs
+++ test/tes_test.exs
@@ -1,8 +1,39 @@
 defmodule TesTest do
   use ExUnit.Case
-  doctest Tes
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  # TODO: Revise these tests with a real CS-generated ESM. At the moment it's a MWedit-generated one.
+  setup do
+    {:ok, stream: Tes.EsmFile.stream("test/fixtures/Test.esm")}
+  end
+
+  test "can read Books successfully", %{stream: stream} do
+    books = Tes.Filter.books(stream)
+
+    assert length(books) == 1
+    assert List.first(books) == %Tes.Book{id: "my_book", name: "My Awesome Book",
+      enchantment_points: 10, weight: 2.5, value: 100, model: "", scroll: false,
+      text: "This book is pretty awesome.", skill_id: 6}
+  end
+
+  test "can read Factions successfully", %{stream: stream} do
+    factions = Tes.Filter.factions(stream)
+
+    assert length(factions) == 1
+    assert List.first(factions) == %Tes.Faction{key: "awesome",
+      name: "Awesome", favorite_attribute_ids: [3, 7],
+      favorite_skill_ids: [9, 18, 19, 4, 17], reactions: [{"awesome", 1}],
+      hidden: true, ranks: [
+        %{name: "Rank A", attribute_1: 10, attribute_2: 10, skill_1: 20, skill_2: 15, faction: 0},
+        %{name: "Rank B", attribute_1: 15, attribute_2: 12, skill_1: 20, skill_2: 20, faction: 10},
+        %{name: "", attribute_1: 0, attribute_2: 0, skill_1: 0, skill_2: 0, faction: 0},
+        %{name: "", attribute_1: 0, attribute_2: 0, skill_1: 0, skill_2: 0, faction: 0},
+        %{name: "", attribute_1: 0, attribute_2: 0, skill_1: 0, skill_2: 0, faction: 0},
+        %{name: "", attribute_1: 0, attribute_2: 0, skill_1: 0, skill_2: 0, faction: 0},
+        %{name: "", attribute_1: 0, attribute_2: 0, skill_1: 0, skill_2: 0, faction: 0},
+        %{name: "", attribute_1: 0, attribute_2: 0, skill_1: 0, skill_2: 0, faction: 0},
+        %{name: "", attribute_1: 0, attribute_2: 0, skill_1: 0, skill_2: 0, faction: 0},
+        %{name: "", attribute_1: 0, attribute_2: 0, skill_1: 0, skill_2: 0, faction: 0}
+      ]
+    }
   end
 end
